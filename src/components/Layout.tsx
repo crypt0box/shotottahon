89baaab6a5e551ecdb7e242ad9e52b491c49a2cd
crypt0box <pray.happy76@gmail.com>
2021-07-11 01:36:06 +0900

    initial commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,79 @@
+import React, { ReactNode } from 'react';
+import {
+  Heading,
+  Flex,
+  Box,
+  IconButton,
+  Button,
+  useDisclosure,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+} from '@chakra-ui/react';
+import { ChevronLeftIcon } from '@chakra-ui/icons';
+
+type Props = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+  children: ReactNode;
+};
+
+export function Layout({ children, ...props }: Props) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const RenderDrawer = (
+    <Drawer placement={'left'} onClose={onClose} isOpen={isOpen}>
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerHeader borderBottomWidth="1px">Basic Drawer</DrawerHeader>
+        <DrawerBody>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </DrawerBody>
+      </DrawerContent>
+    </Drawer>
+  );
+
+  return (
+    <Flex bg="gray.100" w="100vw" h="100vh">
+      <Flex
+        as="header"
+        position="fixed"
+        top={0}
+        width="full"
+        shadow="sm"
+        py={4}
+        px={8}
+        justifyContent="space-between"
+      >
+        <Box>
+          <IconButton
+            aria-label="back"
+            color="black"
+            rounded="full"
+            icon={<ChevronLeftIcon />}
+            onClick={onOpen}
+          />
+        </Box>
+        {RenderDrawer}
+        <Flex alignItems="center">
+          <Heading>Shotottahon</Heading>
+        </Flex>
+        <Box>
+          <Button disabled colorScheme="blue">
+            保存済み
+          </Button>
+        </Box>
+      </Flex>
+      <Box width="100%" mt={'6rem'} {...props}>
+        {children}
+      </Box>
+    </Flex>
+  );
+}
