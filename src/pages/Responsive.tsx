86290613c1f1bf4b0a07e5d33afb398fbfd32186
crypt0box <pray.happy76@gmail.com>
2021-07-11 01:37:21 +0900

    feat: add Responsive page

--- src/pages/Responsive.tsx
+++ src/pages/Responsive.tsx
@@ -2,6 +2,6 @@ import { Box } from '@chakra-ui/react';
 
 export default function Responsive() {
   return (
-    <Box bg={{ base: 'red.200', sm: 'yellow.200', md: 'green.200', lg: 'blue.200' }}>Hello</Box>
+    <Box width="100%" bg={{ base: 'red.200', sm: 'yellow.200', md: 'green.200', lg: 'blue.200' }}>Hello</Box>
   );
 }
