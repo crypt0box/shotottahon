496395e872d7c6c3a05a739e01403a0d15a2f3b0
crypt0box <pray.happy76@gmail.com>
2021-08-01 14:26:11 +0900

    feat: add akutagawa page

--- src/pages/Responsive.tsx
+++
@@ -1,7 +0,0 @@
-import { Box } from '@chakra-ui/react';
-
-export default function Responsive() {
-  return (
-    <Box width="100%" bg={{ base: 'red.200', sm: 'yellow.200', md: 'green.200', lg: 'blue.200' }}>Hello</Box>
-  );
-}
