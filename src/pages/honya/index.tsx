6d2708173ecf5c8a2f9f0d44b5e27db3cb122e33
crypt0box <pray.happy76@gmail.com>
2021-09-25 16:39:44 +0900

    refactor: code refactoring

--- src/pages/honya/index.tsx
+++ src/pages/honya/index.tsx
@@ -1,22 +1,9 @@
 import React, { useState } from 'react';
 import { InferGetStaticPropsType } from 'next';
 import { db } from 'src/lib/db';
-import {
-  Box,
-  Flex,
-  Image,
-  Icon,
-  Button,
-  Link,
-  AlertDialog,
-  AlertDialogOverlay,
-  AlertDialogContent,
-  AlertDialogBody,
-} from '@chakra-ui/react';
-import { ExternalLinkIcon } from '@chakra-ui/icons';
-import { AiFillBook, AiTwotoneTrophy } from 'react-icons/ai';
-import { BsPersonFill } from 'react-icons/bs';
+import { Box, Image } from '@chakra-ui/react';
 import Books from 'src/components/Books';
+import BookDetailModal from 'src/components/BookDetailModal';
 
 type BookProps = {
   id: string;
@@ -49,15 +36,18 @@ export const getStaticProps = async () => {
 };
 
 export default function Akutagawa({ books }: InferGetStaticPropsType<typeof getStaticProps>) {
-  const [selectedBook, setSelectedBook] = useState<string>('');
+  const [book, setBook] = useState<BookProps>(books[0]);
+  const [isOpenDialog, setIsOpenDialog] = useState<boolean>(false);
 
   const onOpenDialog = (id: string | undefined) => {
-    if (!id) return;
-    setSelectedBook(id);
+    const book = books.find((book) => book.id === id);
+    if (!book) return;
+    setBook(book);
+    setIsOpenDialog(true);
   };
 
   const onCloseDialog = () => {
-    setSelectedBook('');
+    setIsOpenDialog(false);
   };
   return (
     <>
@@ -72,69 +62,10 @@ export default function Akutagawa({ books }: InferGetStaticPropsType<typeof getS
               alt={book.title}
               onClick={() => onOpenDialog(book.id)}
             />
-            <AlertDialog
-              isOpen={book.id === selectedBook}
-              onClose={onCloseDialog}
-              leastDestructiveRef={undefined}
-              autoFocus={false}
-              isCentered
-            >
-              <AlertDialogOverlay>
-                <AlertDialogContent>
-                  <AlertDialogBody>
-                    <Flex justifyContent="center">
-                      <Box>
-                        <Flex justifyContent="center">
-                          <Image
-                            maxW="180px"
-                            maxH="256px"
-                            mt="40px"
-                            mb="20px"
-                            boxShadow="-6px 6px 10px -2px rgb(0 27 68 / 25%), 0 0 3px rgb(0 21 60 / 10%)"
-                            boxSize="100%"
-                            objectFit="fill"
-                            src={book.imageUrl}
-                            alt={book.title}
-                          />
-                        </Flex>
-                        <Box my="5px">
-                          <Icon as={AiFillBook} mr="15px" />
-                          {book.title}
-                        </Box>
-                        <Box my="5px">
-                          <Icon as={BsPersonFill} mr="15px" />
-                          {book.author}
-                        </Box>
-                        <Box my="5px">
-                          <Icon as={AiTwotoneTrophy} mr="15px" />
-                          {book.reward}
-                        </Box>
-                        <Box mt="20px">
-                          <Link href={book.link} isExternal _hover={{ textDecoration: 'none' }}>
-                            <Button
-                              leftIcon={<ExternalLinkIcon />}
-                              w="100%"
-                              color="white"
-                              colorScheme="red"
-                            >
-                              楽天で詳細をみる
-                            </Button>
-                          </Link>
-                        </Box>
-                        <Box mt="10px" mb="20px">
-                          <Button w="100%" onClick={onCloseDialog}>
-                            閉じる
-                          </Button>
-                        </Box>
-                      </Box>
-                    </Flex>
-                  </AlertDialogBody>
-                </AlertDialogContent>
-              </AlertDialogOverlay>
-            </AlertDialog>
           </Box>
         ))}
       </Books>
+      <BookDetailModal isOpenDialog={isOpenDialog} onCloseDialog={onCloseDialog} book={book} />
     </>
   );
 }
